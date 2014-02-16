d5260b85babe133aa3aa299b0c53c31e204400a4
Justin Liu <yuting1987@gmail.com>
2014-02-16 13:50:42 +0800

    CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
-    through2 = require('through2'),
-    clean = require('gulp-clean');
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean'),
+    cssmin = require('gulp-minify-css');
+
 
 process.on('uncaughtException', function (err) {
     console.error(err);
@@ -13,40 +15,43 @@ process.on('uncaughtException', function (err) {
 
 paths = {
     src: 'src',
+    temp: 'temp',
     dest: 'build'
 }
 
-
-gulp.task('grandfather', function(){
-    return gulp.src('*')
-               .pipe(through2.obj(function(file, enc, cb){
-                    setTimeout(cb, 1000);
-                    // console.log("I'm Mom");
-               }));
+gulp.task('cleancss', function(){
+    return gulp.src([paths.dest + '/style/*.css', paths.temp + '/style/*.css'])
+               .pipe(clean());
 });
 
-gulp.task('mon', ['grandfather'], function(){
-    return gulp.src('*')
-               .pipe(through2.obj(function(file, enc, cb){
-                    setTimeout(cb, 500);
-                    // console.log("I'm Mom");
-               }));
+gulp.task('sass', ['cleancss'], function () {
+    return gulp.src(paths.src + '/style/*.sass')
+        .pipe(compass({
+            css: 'src/css',
+            sass: 'src/style',
+            image: 'src/images'
+            // logging: false
+        }))
+        .pipe(gulp.dest(paths.temp + '/style'));
 });
 
-gulp.task('dad', function(){
-    return gulp.src('*')
-               .pipe(through2.obj(function(file, enc, cb){
-                    setTimeout(cb, 500);
-                    // console.log("I'm Dad");
-               }));
+gulp.task('stylus', ['cleancss'], function () {
+    return gulp.src(paths.src + '/style/*.styl')
+        .pipe(stylus({use: ['nib']}))
+        .pipe(gulp.dest(paths.temp + '/style'));
 });
 
-gulp.task('me', ['mon', 'dad'], function(){
-    return gulp.src('*')
-               .pipe(through2.obj(function(file, enc, cb){
-                    setTimeout(cb, 200);
-                    // console.log("I'm Me");
-               }));
+gulp.task('css', ['sass', 'stylus'], function(){
+    return gulp.src(paths.temp + '/style/*.css')
+        .pipe(concat("all.css"))
+        .pipe(gulp.dest(paths.dest + '/style/'))
 });
 
-gulp.task('default', ['me']);
+gulp.task('cssmin', ['css'], function(){
+    return gulp.src(paths.dest + '/style/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest(paths.dest + '/style/'))
+});
+
+gulp.task('default', ['css']);
+gulp.task('build', ['cssmin']);
