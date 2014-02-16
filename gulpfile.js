a580df9d24cebd90ecd65d3b96a50c7361958f79
Justin Liu <yuting1987@gmail.com>
2014-02-16 11:12:40 +0800

    優先權

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,52 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
+    through2 = require('through2'),
     clean = require('gulp-clean');
 
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    var tasks = process.argv.slice(2);
+    tasks.length || tasks.push('default');
+    gulp.start.apply(gulp, tasks);
+});
+
 paths = {
-  src: 'src',
-  dest: 'build'
+    src: 'src',
+    dest: 'build'
 }
 
-gulp.task('script_clean', function() {
-  return gulp.src([paths.dest + '/*.js', paths.dest + '/**/*.js'], {read: false})
-             .pipe(clean());
-});
-gulp.task('script', ['script_clean'], function() {
-  return gulp.src([paths.src + '/*.js', paths.src + '/**/*.js'])
-             .pipe(gulp.dest(paths.dest));
+
+gulp.task('grandfather', function(){
+    return gulp.src('*')
+               .pipe(through2.obj(function(file, enc, cb){
+                    setTimeout(cb, 1000);
+                    // console.log("I'm Mom");
+               }));
 });
 
-gulp.task('html_clean', function() {
-  return gulp.src([paths.dest + '/*.html', paths.dest + '/**/*.html'], {read: false})
-             .pipe(clean());
-});
-gulp.task('jade', ['html_clean'], function() {
-  return gulp.src([paths.src + '/*.jade', paths.src + '/**/*.jade'])
-             .pipe(jade())
-             .pipe(gulp.dest(paths.dest));
+gulp.task('mon', ['grandfather'], function(){
+    return gulp.src('*')
+               .pipe(through2.obj(function(file, enc, cb){
+                    setTimeout(cb, 500);
+                    // console.log("I'm Mom");
+               }));
 });
 
-gulp.task('style_clean', function() {
-  return gulp.src([paths.dest + '/*.css', paths.dest + '/**/*.css'], {read: false})
-             .pipe(clean());
-});
-gulp.task('stylus', ['style_clean'], function() {
-  return gulp.src([paths.src + '/*.styl', paths.src + '/**/*.styl'])
-             .pipe(stylus())
-             .pipe(gulp.dest(paths.dest));
-});
-gulp.task('style', ['style_clean'], function() {
-  return gulp.src([paths.src + '/*.css', paths.src + '/**/*.css'])
-             .pipe(gulp.dest(paths.dest));
+gulp.task('dad', function(){
+    return gulp.src('*')
+               .pipe(through2.obj(function(file, enc, cb){
+                    setTimeout(cb, 500);
+                    // console.log("I'm Dad");
+               }));
 });
 
-gulp.task('fonts', function() {
-  return gulp.src([paths.src + '/fonts/*'])
-             .pipe(gulp.dest(paths.dest + '/fonts/'));
+gulp.task('me', ['mon', 'dad'], function(){
+    return gulp.src('*')
+               .pipe(through2.obj(function(file, enc, cb){
+                    setTimeout(cb, 200);
+                    // console.log("I'm Me");
+               }));
 });
 
-gulp.task('image', function() {
-  return gulp.src([paths.src + '/image/*'])
-             .pipe(gulp.dest(paths.dest + '/image/'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch([paths.src + '/image/*'], ['image']);
-  gulp.watch([paths.src + '/*.js', paths.src + '/**/*.js'], ['script']);
-  gulp.watch([paths.src + '/*.jade', paths.src + '/**/*.jade'], ['jade']);
-  gulp.watch([
-    paths.src + '/*.styl', paths.src + '/**/*.styl',
-    paths.src + '/*.css', paths.src + '/**/*.css'
-    ], ['stylus','style']);
-});
-
-// // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['script','jade','stylus','style','fonts','image','watch']);
+gulp.task('default', ['me']);
